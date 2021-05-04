2dd907fde5a4be9e64cec1ee874fc652a1160b41
Den <83018062+DenSidorov@users.noreply.github.com>
2021-05-04 18:44:49 +0300

    del save in file

--- new_cats.py
+++ new_cats.py
@@ -2,7 +2,6 @@ from cat import Cat
 
 cat_1 = Cat("Барон", "Мальчик", "2 года", "Без Дома")
 cat_2 = Cat("Сэм", "Мальчик", "2 года", "Без Дома")
-cat_1.save()
-cat_2.save()
+
 print(cat_1.get_name(), cat_1.get_gender(), cat_1.get_age(), cat_1.get_status())
 print(cat_2.get_name(), cat_2.get_gender(), cat_2.get_age(), cat_2.get_status())
